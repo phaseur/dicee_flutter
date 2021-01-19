efabe4917b666e1b9b554a6be890fbfcbfe1e9a8
Phaseur <nico7798@gmail.com>
2021-01-19 19:34:41 +0100

    adding dicee and FlatButton

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(
+            child: Text('Dicee'),
+          ),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -16,8 +18,27 @@ void main() {
 }
 
 class DicePage extends StatelessWidget {
+  static const int randomLeftNumber = 3;
+  static const int randomRightNumber = 6;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(child: Image.asset('images/dice$randomLeftNumber.png'), onPressed: (){
+              print('left button is pressed');
+            },),
+          ),
+          Expanded(
+              child: FlatButton(onPressed: (){
+                print('right button is pressed');
+              },child: Image.asset('images/dice$randomRightNumber.png'))
+          ),
+        ],
+      ),
+    );
   }
 }
+
+// 'flutter pub get' in the current terminal to get dependencies
